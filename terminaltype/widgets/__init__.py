c3686590da30b86033a11a7fd09c5c52bdd1f969
Adam Vinch <adamvinch@gmail.com>
2023-10-09 18:01:47 -0400

    Add MenuOption as a recordtype

--- terminaltype/widgets/__init__.py
+++ terminaltype/widgets/__init__.py
@@ -1,6 +1,8 @@
-
+from recordtype import recordtype
 
 
 SYMBOLS = {'toggle-off': '\uf204',
-           'toggle-on': '\uf205'
-        }
+           'toggle-on': '\uf205'}
+
+MenuOption = recordtype('MenuOption', 'name is_selected')
+
