098f3c4508406e7816e32d2b31490b05b81af06c
Adam Vinch <adamvinch@gmail.com>
2023-10-10 01:34:20 -0400

    Set up structure for actual typing test class

---
+++ terminaltype/typingtest/test_types.py
@@ -0,0 +1,21 @@
+from terminaltype.typingtest.typing_test import TypingTest
+
+
+class FifteenSecondTest(TypingTest):
+    pass
+
+
+class ThirtySecondTest(TypingTest):
+    pass
+
+
+class MinuteTest(TypingTest):
+    pass
+
+
+class ThirtyWordTest(TypingTest):
+    pass
+
+
+class OneHundredWordTest(TypingTest):
+    pass
---
+++ terminaltype/typingtest/typing_test.py
@@ -0,0 +1,8 @@
+
+
+class TypingTest:
+    def __init__(self):
+        pass
+
+    def generate_new_test(self) -> str:
+        return ""
